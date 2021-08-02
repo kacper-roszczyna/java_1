ca7a5f79652408dcbebaf569a2c046093765adae
Kacper Roszczyna <kacper.roszczyna@gmail.com>
2021-08-03 00:59:09 +0200

    commented out test not to be used at this stage

--- src/test/java/pl/roszczyna/teaching/TeachingApplicationTests.java
+++ src/test/java/pl/roszczyna/teaching/TeachingApplicationTests.java
@@ -3,11 +3,11 @@ package pl.roszczyna.teaching;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class TeachingApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class TeachingApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
